e4d7693e3ccd682cfdde98f1aa6458f88ffd88c1
yana7777777 <yana_77_70@mail.ru>
2025-03-25 21:49:02 +0400

    new device

--- main1.py
+++ main1.py
@@ -1,24 +1,26 @@
 # 1-й вар
-a = 1
-b = 2
-print("a: ", a)
-print("b: ", b)
-a, b = b, a
-print("a: ", a)
-print("b: ", b)
+# a = 1
+# b = 2
+# print("a: ", a)
+# print("b: ", b)
+# a, b = b, a
+# print("a: ", a)
+# print("b: ", b)
+#
+# # 2-й вар
+# a = 1
+# b = 2
+# print("a: ", a)
+# print("b: ", b)
+#
+# c = a
+# a = b
+# b = c
+#
+# print("a: ", a)
+# print("b: ", b)
+# # print("c: ", c)
+#
+# print("Текст в локальном репозитории")
 
-# 2-й вар
-a = 1
-b = 2
-print("a: ", a)
-print("b: ", b)
-
-c = a
-a = b
-b = c
-
-print("a: ", a)
-print("b: ", b)
-# print("c: ", c)
-
-print("Текст в локальном репозитории")
+print("Код написан на новом устройстве")
