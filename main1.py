76fab9aa3e4414acb62aac4c1a83b5442e50c804
yana7777777 <yana_77_70@mail.ru>
2025-03-25 22:35:45 +0400

    new change text

--- main1.py
+++ main1.py
@@ -23,4 +23,21 @@
 #
 # print("Текст в локальном репозитории")
 
-print("Код написан на новом устройстве")
+# print("Код написан на новом устройстве")
+
+# f = open("text.txt")
+# f = open(r"C:\Users\ASUS\PycharmProjects\PythonProject2\text.txt", "r")
+# print(*f)
+# print(f)
+# print(f.mode)
+# print(f.name)
+# print(f.encoding)
+# f.close()
+# print(f.closed)
+
+
+f = open("text.txt")
+print(f.read(3))
+print(f.read())
+f.close()
+
