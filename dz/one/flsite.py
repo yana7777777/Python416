d19c9636d7f769f145918ee34c909d0de9dec45d
yana7777777 <yana_77_70@mail.ru>
2025-06-20 23:22:57 +0400

    added dz_39_bd7

---
+++ dz/one/flsite.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+menu = [
+    {"name": "Главная", "url": "index"},
+    {"name": "О нас", "url": "about"},
+    {"name": "Обратная связь", "url": "contact"}
+]
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html", title="Главная", menu=menu)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="О нас", menu=menu)
+
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        print(request.form)
+    return render_template("contact.html", title="Обратная связь", menu=menu)
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    return f"Пользователь: {username}"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
